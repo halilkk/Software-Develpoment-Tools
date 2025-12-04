f54a285bea97d6f38992ca814fc36a201f67c957
aesir._.balder <yapicifurkan47@gmail.com>
2025-12-04 17:15:38 +0300

    Update main.cpp
    For Software Homework

--- Homework/Homework/main.cpp
+++ Homework/Homework/main.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 
-
+//Edited By Furkan Yapici For Homework
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";

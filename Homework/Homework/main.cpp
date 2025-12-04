e3af3bf5190d002db4ebd159a7ff85e64afd974f
Halil Köker <hkoker444@gmail.com>
2025-12-04 14:29:05 +0100

    Extended Code

--- Homework/Homework/main.cpp
+++ Homework/Homework/main.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
-#include <cstdlib> // rand, srand icin
-#include <ctime>   // time icin
+#include <cstdlib>
+#include <ctime>
+
+// Diziyi yazdiran basit bir fonksiyon
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+// i) Manuel Bubble Sort Algoritmasi
+void simpleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Yer degistirme
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int main() {
-    // 5 elemanli basit bir C dizisi
     int numbers[5];
     int n = 5;
 
     std::srand(std::time(0));
 
-    std::cout << "Original Array Elements:" << std::endl;
-
-    // g) Diziyi rastgele degerlerle baslat
+    // Rastgele deger atama (g)
     for (int i = 0; i < n; ++i) {
         numbers[i] = std::rand() % 100;
-        std::cout << "Index " << i << ": " << numbers[i] << std::endl;
     }
+
+    std::cout << "--- Original Array ---" << std::endl;
+    printArray(numbers, n);
+
+    // Diziyi siralama
+    simpleSort(numbers, n);
+
+    std::cout << "--- Sorted Array (Bubble Sort) ---" << std::endl;
+    printArray(numbers, n);
 
     return 0;
 }
